b314ed50ecc3e4d675236eab7d6bb5d9e988b3ec
Xiaoxi <44510475+greenlexie@users.noreply.github.com>
2020-12-07 18:05:55 -0600

    Add files via upload

---
+++ Python Automation AB Testing.py
@@ -0,0 +1,742 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Python Automation for AB Test
+# 
+# 
+# ## Setup
+# ### Load Libraries & Data
+
+# In[140]:
+
+
+##import Packages
+import pandas as pd
+import math
+import scipy.stats
+from matplotlib import pyplot as plt
+import numpy as np
+
+
+# In[199]:
+
+
+##import data
+# your file needs to be in the right file path
+df_test2 = pd.read_csv(r'MTA AB Test2.csv')
+df_rev= pd.read_csv(r'MTA AB Test2 Rev.csv')
+#df_cateforymap=pd.read_csv(r'MTA AB Category Mapping.csv')
+#df_test_overlapped=pd.read_csv(r'C:\ProgramData\MySQL\MySQL Server 8.0\Uploads\MTA AB Visit Data Test 3 -- overlapped.csv')
+# ^ this is for when you want to study overlaps: you can export data from MySQL workbench
+# you can also set your export file path as your current directory
+
+
+# ### Data Check & Transformation
+# #### Inspect DF Statistics
+
+# In[200]:
+
+
+df_test2.describe()
+
+
+# In[143]:
+
+
+df_test2
+
+
+# #### Session Count By Date
+
+# In[201]:
+
+
+df_test2.drop_duplicates(['Date','SessionID']).groupby(['Date'])['SessionID'].count()
+
+
+# In[202]:
+
+
+df_test2=df_test2[pd.to_datetime(df_test2['Date'])>pd.to_datetime('2019-06-14')]
+
+
+# In[203]:
+
+
+df_test2
+
+
+# In[204]:
+
+
+# garbage collect, clears memory space for local machine efficiency
+import gc
+gc.collect()
+
+
+# ### Z-score Calculation
+
+# In[205]:
+
+
+# list all the metrics
+metrics=['Bounced','SawProduct','AddedToCart','ReachedCheckout','Converted']
+
+
+# In[206]:
+
+
+df_Result=pd.DataFrame(df_test2.drop_duplicates(['SessionID','ControlGroup']).groupby('TestGroup')['SessionID'].count())
+
+
+# In[207]:
+
+
+df_Result.head(10)
+
+
+# In[208]:
+
+
+# add aggregated metrics using a for loop
+
+for metric in metrics:
+    a=df_test2[df_test2[metric]==1].drop_duplicates(['SessionID','ControlGroup']).groupby('TestGroup')['SessionID'].count()
+    a.name=metric
+    df_Result=df_Result.join(a)
+
+
+# In[209]:
+
+
+df_Result
+
+
+# In[210]:
+
+
+def z_test_calculator(df,denominator,numerator):
+    #df: df_Result - dataframe containing the result (absolute counts) - this is the df_result from previous step
+    #denominator: str (column name)
+    #numerator:str
+    
+    
+    #get the data, and transform
+    #https://www.w3resource.com/pandas/dataframe/dataframe-loc.php#:~:text=DataFrame%20%2D%20loc%20property,used%20with%20a%20boolean%20array.
+    control_denominator=df.loc[0,denominator]
+    var_denominator=df.loc[1,denominator]
+    control_numerator=df.loc[0,numerator]
+    var_numerator=df.loc[1,numerator]    
+    
+    #Rate
+    control_rate=control_numerator/control_denominator
+
+    var_rate=var_numerator/var_denominator
+    
+    #STD
+    control_sd=math.sqrt(control_rate*(1-control_rate)/control_denominator)
+    var_sd=math.sqrt(var_rate*(1-var_rate)/var_numerator)
+    
+    #z score
+    z_score=(control_rate-var_rate)/math.sqrt(pow(control_sd,2)+pow(var_sd,2))
+    
+    #p value
+    p_value=scipy.stats.norm.sf(abs(z_score))
+    
+    #lift
+    perc_lift=(var_rate-control_rate)/control_rate
+    abs_lift=(var_rate-control_rate)
+    
+    return (p_value,perc_lift,abs_lift)
+
+
+# In[211]:
+
+
+z_test_calculator(df_Result,'SessionID','Converted')
+
+
+# In[212]:
+
+
+z_test_calculator(df_Result,'SessionID','ReachedCheckout')
+
+
+# ### Format output
+
+# In[213]:
+
+
+KPIs=[('SessionID','ReachedCheckout') #tuple
+      ,('SessionID','Converted')]
+
+
+# In[214]:
+
+
+df_Result.index
+
+
+# In[215]:
+
+
+dic_final={}
+
+
+# In[216]:
+
+
+for index in df_Result.index:
+    j=0 #initializize counter
+    if index!=0:
+        df_each_group=df_Result.loc[[0,index],]
+        df_each_group.index=[1,0]
+        
+        # this is a dataframe, otherwise the df_fianl.loc lines won't work
+        df_final=pd.DataFrame()
+        
+        for i in KPIs:
+            result=z_test_calculator(df_each_group,i[0],i[1])
+            df_final.loc[j,'denominator']=i[0]
+            df_final.loc[j,'numerator']=i[1]
+            df_final.loc[j,'p_value']=result[0]
+            df_final.loc[j,'perc_lift']=result[1]
+            df_final.loc[j,'abs_lift']=result[2]
+            j=j+1
+        dic_final['Variation '+str(index)]=df_final
+
+
+# In[217]:
+
+
+dic_final['Variation 1']
+
+
+# In[218]:
+
+
+dic_final.keys()
+
+
+# ### Export to Excel
+
+# In[162]:
+
+
+writer = pd.ExcelWriter('Test2 Session Overall Results.xlsx')
+for key in dic_final.keys():
+    dic_final[key].to_excel(writer,sheet_name=key)
+writer.save()
+
+
+# ## Week 6: Granularity, Cuts & Continunous Variables
+# We are going to perform both the Mann-Whitney U Test and a Z-score calculation
+
+# ### Granularity
+# Set a variable for CUsID
+
+# In[219]:
+
+
+granularity='CusID'
+
+
+# In[220]:
+
+
+df_Result=pd.DataFrame(df_test2.drop_duplicates([granularity,'ControlGroup']).groupby('TestGroup')[granularity].count())
+
+for metric in metrics:
+    a=df_test2[df_test2[metric]==1].drop_duplicates([granularity,'ControlGroup']).groupby('TestGroup')[granularity].count()
+    a.name=metric
+    df_Result=df_Result.join(a)
+
+KPIs_cus=[(granularity,'Bounced'),
+      (granularity,'Converted'),
+      (granularity,'AddedToCart'),
+      (granularity,'ReachedCheckout'),
+      (granularity,'SawProduct'),
+      ('SawProduct','Bounced'),
+      ('SawProduct','AddedToCart'),
+      ('AddedToCart','ReachedCheckout'),
+      ('ReachedCheckout','Converted')]
+
+
+# In[221]:
+
+
+for index in df_Result.index:
+    j=0
+    if index!=0:
+        df_each_group=df_Result.loc[[0,index],]
+        df_each_group.index=[1,0]
+        
+        df_final=pd.DataFrame() # your original df_final will disappear
+        
+        for i in KPIs_cus:
+            result=z_test_calculator(df_each_group,i[0],i[1])
+            df_final.loc[j,'denominator']=i[0]
+            df_final.loc[j,'numerator']=i[1]
+            df_final.loc[j,'p_value']=result[0]
+            df_final.loc[j,'perc_lift']=result[1]
+            df_final.loc[j,'abs_lift']=result[2]
+            j=j+1
+        dic_final['Variation'+str(index)]=df_final
+
+
+# In[222]:
+
+
+df_Result
+
+
+# In[223]:
+
+
+dic_final
+
+
+# ### Cuts by Category
+
+# In[224]:
+
+
+cut='VisitorTypeID' # define a dimension to cut the data by
+granularity='SessionID'
+
+df_test_data=df_test2.copy()
+
+for p in set(df_test_data[cut]):
+    # isolate each cut for aggregation
+    df_test2=df_test_data[df_test_data[cut]==p]
+    
+    df_Result=pd.DataFrame(df_test2.groupby('TestGroup')[granularity].count())
+    metrics=['Bounced','SawProduct','AddedToCart','ReachedCheckout','Converted']
+    
+    # create for loops within the loop for cuts
+    for metric in metrics:
+        a=df_test2[df_test2[metric]==1].drop_duplicates([granularity,'ControlGroup']).groupby('TestGroup')[granularity].count()
+        a.name=metric
+        df_Result=df_Result.join(a)
+
+    KPIs=[(granularity,'Bounced'),
+          (granularity,'Converted'),
+          (granularity,'AddedToCart'),
+          (granularity,'ReachedCheckout'),
+          (granularity,'SawProduct'),
+          ('SawProduct','Bounced'),
+          ('SawProduct','AddedToCart'),
+          ('AddedToCart','ReachedCheckout'),
+          ('ReachedCheckout','Converted')]
+    
+    for index in df_Result.index:
+        j=0
+        if index!=0:
+            df_each_group=df_Result.loc[[0,index],]
+            df_each_group.index=[1,0]
+
+            df_final=pd.DataFrame()
+
+            for i in KPIs:
+                result=z_test_calculator(df_each_group,i[0],i[1])
+                df_final.loc[j,'denominator']=i[0]
+                df_final.loc[j,'numerator']=i[1]
+                df_final.loc[j,'p_value']=result[0]
+                df_final.loc[j,'perc_lift']=result[1]
+                df_final.loc[j,'abs_lift']=result[2]
+                j=j+1
+            dic_final['Variation'+str(index)+'cut'+str(p)]=df_final
+
+
+# In[225]:
+
+
+dic_final
+
+
+# In[226]:
+
+
+writer=pd.ExcelWriter('output'+cut+'.xlsx')
+for key in dic_final.keys():
+    dic_final[key].to_excel(writer, sheet_name=key)
+writer.save()
+
+
+# ### Continuous Variable Significance
+# #### Mann-Whitney U Test
+
+# In[175]:
+
+
+# Mann-Whitney test only reads arrays
+Control_Rev=df_rev[df_rev['TestGroup']==0]['TotalRev'].array
+Var1_Rev=df_rev[df_rev['TestGroup']==1]['TotalRev'].array
+
+
+# In[176]:
+
+
+Control_Rev # data type is float64
+
+
+# In[177]:
+
+
+Var1_Rev
+
+
+# In[178]:
+
+
+# note: we did not abbreviate the name of the package in the beginning
+import scipy.stats as stats
+
+
+# In[179]:
+
+
+stats.mannwhitneyu(Control_Rev,Var1_Rev)
+
+
+# #### Normal Distribution Z-score Calculator
+
+# In[119]:
+
+
+sum(Control_Rev)
+
+
+# In[120]:
+
+
+sum(Var1_Rev)
+
+
+# In[121]:
+
+
+df_Result
+
+
+# In[122]:
+
+
+# re-declare df_Test
+df_test2 = pd.read_csv(r'MTA AB Test2.csv')
+df_test2 = df_test2[pd.to_datetime(df_test2['Date'])>pd.to_datetime('2019-06-14')]
+
+# re-declare df_Result, so that the results are not cut by category
+df_Result=pd.DataFrame(df_test2.groupby('TestGroup')['SessionID'].count())
+metrics=['Bounced','SawProduct','AddedToCart','ReachedCheckout','Converted']
+
+gc.collect()
+
+for metric in metrics:
+    a=df_test2[df_test2[metric]==1].groupby('TestGroup')['SessionID'].count()
+    a.name=metric
+    df_Result=df_Result.join(a)
+
+
+# In[123]:
+
+
+# transform the arrays to df columns to get sum total 
+df_Result.loc[0,'Rev']=sum(Control_Rev)
+df_Result.loc[1,'Rev']=sum(Var1_Rev)
+# add sum squared total
+df_Result.loc[0,'Rev_sq']=sum(Control_Rev**2) # x1^2+x2^2+...+xn^2
+df_Result.loc[1,'Rev_sq']=sum(Var1_Rev**2)
+
+
+# In[124]:
+
+
+df_Result
+
+
+# In[125]:
+
+
+# if you want to surpress the scientific notation
+# pd.options.display.float_format='{:.2f}'.format
+df_Result
+# if you don't like it you can also reset the option 
+# pd.reset_option('^display.', silent=True)
+
+
+# In[126]:
+
+
+def z_test_calculator_continuous(df,denominator,numerator,numerator_sq):
+    #df: df_Result - dataframe containing the result (absolute counts) - this is the df_result from previous step
+    #denominator: str (column name)
+    #numerator:str
+    #numerator_sq:str
+    
+    
+    #get the data, and transform
+    #https://www.w3resource.com/pandas/dataframe/dataframe-loc.php#:~:text=DataFrame%20%2D%20loc%20property,used%20with%20a%20boolean%20array.
+    control_denominator=df.loc[0,denominator]
+    var_denominator=df.loc[1,denominator]
+    control_numerator=df.loc[0,numerator]
+    var_numerator=df.loc[1,numerator]    
+    
+    #Rate: actually not a rate, but an average
+    control_rate=control_numerator/control_denominator
+    var_rate=var_numerator/var_denominator
+    
+    #Variance
+    control_var=df.loc[0,numerator_sq]/control_denominator-control_rate**2
+    var_var=df.loc[1,numerator_sq]/var_denominator-var_rate**2
+    
+    #z score
+    z_score=(control_rate-var_rate)/math.sqrt(control_var/control_denominator+var_var/var_denominator)
+    
+    #p value
+    p_value=scipy.stats.norm.sf(abs(z_score))
+    
+    #lift
+    perc_lift=(var_rate-control_rate)/control_rate
+    abs_lift=(var_rate-control_rate)
+    
+    return (p_value,perc_lift,abs_lift)
+
+
+# In[127]:
+
+
+# declare a new dictionary for the revenue metrics
+dic_rev_final={}
+for index in df_Result.index:
+    j=0
+    if index!=0:
+        df_each_group=df_Result.loc[[0,index],]
+        df_each_group.index=[1,0]
+        df_final=pd.DataFrame()
+        result=z_test_calculator_continuous(df_each_group,'Converted','Rev','Rev_sq')
+        df_final.loc[j,'denominator']='Converted'
+        df_final.loc[j,'numerator']='Rev'
+        df_final.loc[j,'p_value']=result[0]
+        df_final.loc[j,'perc_lift']=result[1]
+        df_final.loc[j,'abs_lift']=result[2]
+        j=j+1
+        dic_rev_final['Variation '+str(index)]= df_final
+
+
+# In[128]:
+
+
+dic_rev_final
+
+
+# ## Visualizations
+# ### Outliers
+
+# In[129]:
+
+
+# you've already imported these
+# import numpy as np
+# import matplotlib.pyplot as plt
+# Ctrl+/ (windows) or Cmnd+/ (mac) for block comment
+
+# transform control data, and normalize skewed & large values
+x=df_rev.loc[df_rev['ControlGroup']==1,'TotalRev'].apply(lambda x: math.sqrt(abs(x)))
+n_bins=50
+fig,ax=plt.subplots(figsize=(8,4))
+# fig: your figure/image object
+# ax: your axes object - store and manipulate
+# https://stackoverflow.com/questions/34162443/why-do-many-examples-use-fig-ax-plt-subplots-in-matplotlib-pyplot-python
+n,bins,patches=ax.hist(x, n_bins,density=True,histtype='step',cumulative=True,label='Cumulative') # this is a CDF plot
+
+# label, title etc.
+ax.grid(True)
+ax.legend(loc='right')
+ax.set_title('Cumulative step histograms')
+ax.set_xlabel('Squared Total Revenue')
+ax.set_ylabel('Likelihood of occurrence')
+
+plt.show()
+
+
+# In[130]:
+
+
+# get the 95th percentile of each group
+P1=np.percentile(Control_Rev,95)
+P2=np.percentile(Var1_Rev,95)
+
+
+# In[131]:
+
+
+P1
+
+
+# In[132]:
+
+
+P2
+
+
+# In[133]:
+
+
+np.percentile(Control_Rev,[5,95])
+
+
+# In[134]:
+
+
+stats.mannwhitneyu(Control_Rev[Control_Rev<P1],Var1_Rev[Var1_Rev<P2])
+
+
+# In[135]:
+
+
+# change the input of z-score calculator to exclude 95th percentile outliers
+df_Result.loc[0,'Rev']=sum(Control_Rev[Control_Rev<P1])
+df_Result.loc[1,'Rev']=sum(Var1_Rev[Var1_Rev<P2])
+df_Result.loc[0,'Rev_sq']=sum(Control_Rev[Control_Rev<P1]**2)
+df_Result.loc[1,'Rev_sq']=sum(Var1_Rev[Var1_Rev<P2]**2)
+
+
+# In[138]:
+
+
+df_Result
+
+
+# In[137]:
+
+
+z_test_calculator_continuous(df_Result,Converted,Rev,Rev_sq)
+
+
+# In[139]:
+
+
+# declare a new dictionary for the revenue metrics
+dic_rev_final={}
+for index in df_Result.index:
+    j=0
+    if index!=0:
+        df_each_group=df_Result.loc[[0,index],]
+        df_each_group.index=[1,0]
+        df_final=pd.DataFrame()
+        result=z_test_calculator_continuous(df_each_group,'Converted','Rev','Rev_sq')
+        df_final.loc[j,'denominator']='Converted'
+        df_final.loc[j,'numerator']='Rev'
+        df_final.loc[j,'p_value']=result[0]
+        df_final.loc[j,'perc_lift']=result[1]
+        df_final.loc[j,'abs_lift']=result[2]
+        j=j+1
+        dic_rev_final['Variation '+str(index)]= df_final
+
+
+# In[66]:
+
+
+dic_rev_final
+
+
+# ### Significance
+
+# In[180]:
+
+
+numerator='Converted'
+denominator='ReachedCheckout'
+
+
+# In[181]:
+
+
+df_test_data=df_test2.copy() # re-declare or rename
+
+
+# In[182]:
+
+
+denominator_dailydata=df_test_data[df_test_data[denominator]==1].groupby(['Date','TestGroup'])['SessionID'].count()
+numerator_dailydata=df_test_data[df_test_data[numerator]==1].groupby(['Date','TestGroup'])['SessionID'].count()
+numerator_dailydata.name=numerator
+denominator_dailydata.name=denominator
+
+
+# In[183]:
+
+
+type(numerator_dailydata)
+
+
+# In[184]:
+
+
+# same values, but we turn series into dataframes for further processing
+denominator_aggdailydata=denominator_dailydata.reset_index()
+numerator_aggdailydata=numerator_dailydata.reset_index()
+
+
+# In[185]:
+
+
+denominator_aggdailydata
+
+
+# In[186]:
+
+
+# get cumulative value as days progress
+denominator_aggdailydata[denominator]=denominator_aggdailydata.groupby('TestGroup')[denominator].cumsum()
+numerator_aggdailydata[numerator]=numerator_aggdailydata.groupby('TestGroup')[numerator].cumsum()
+
+
+# In[192]:
+
+
+numerator_aggdailydata
+
+
+# In[188]:
+
+
+df_cumsum=pd.DataFrame()
+
+# loop through every unique date
+for date in df_test_data.drop_duplicates('Date')['Date'].tolist():
+    # get data for current loop date
+    df=denominator_aggdailydata[denominator_aggdailydata['Date']==date].set_index('TestGroup',drop=True)
+    df=df.merge(numerator_aggdailydata[numerator_aggdailydata['Date']==date].set_index('TestGroup',drop=True)
+                ,on='Date',left_index=True,right_index=True)
+    
+    sig_result=z_test_calculator(df,denominator, numerator)
+    df_cumsum.loc[date,'Sig_Level']=1-sig_result[0]
+    df_cumsum.loc[date,'Lift']=sig_result[1]
+
+
+# In[189]:
+
+
+df_cumsum
+
+
+# In[190]:
+
+
+ax = df_cumsum.sort_index()['Sig_Level'].plot(figsize=(15,5))
+ax.axhline(y=0.95,linewidth=1,color='r')
+
+
+# In[191]:
+
+
+ax = df_cumsum.sort_index()['Sig_Level'].plot(figsize=(15,5))
+
+
+# In[ ]:
+
+
+
+
